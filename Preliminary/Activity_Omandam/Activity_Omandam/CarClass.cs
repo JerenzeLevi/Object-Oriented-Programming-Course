89d477087d41388b3e12acf84d4353631e9b39ff
The Knave <omandamjerenze@gmail.com>
2026-05-21 17:45:51 +0800

    Added a file

---
+++ Preliminary/Activity_Omandam/Activity_Omandam/CarClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_Omandam
+{
+    class CarClass
+    {
+        string carModel = "Lamborghini Sesto Elemento";
+        string carBrand = "Lamborghini";
+        string carColor = "Pink";
+        string carWeight = "999kg";
+
+        public void accelerate()
+        {
+
+            MessageBox.Show("The car is accelerating");
+        }
+    }
+}
