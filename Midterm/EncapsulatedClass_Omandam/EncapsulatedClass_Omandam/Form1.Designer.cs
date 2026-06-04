84799ef70c1a6687914f6c3dbff26501e3a1691d
The Knave <omandamjerenze@gmail.com>
2026-06-04 18:55:05 +0800

    Added a file

---
+++ Midterm/EncapsulatedClass_Omandam/EncapsulatedClass_Omandam/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace EncapsulatedClass_Omandam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtfullname = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtage = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnationality = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtcivilstatus = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbirthdate = new Guna.UI2.WinForms.Guna2TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clearbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtfullname
+            // 
+            this.txtfullname.BackColor = System.Drawing.Color.Transparent;
+            this.txtfullname.BorderColor = System.Drawing.Color.LawnGreen;
+            this.txtfullname.BorderRadius = 10;
+            this.txtfullname.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.txtfullname.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtfullname.DefaultText = "";
+            this.txtfullname.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtfullname.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtfullname.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtfullname.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtfullname.FillColor = System.Drawing.Color.Azure;
+            this.txtfullname.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtfullname.Font = new System.Drawing.Font("Garamond", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfullname.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtfullname.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtfullname.Location = new System.Drawing.Point(125, 59);
+            this.txtfullname.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtfullname.Name = "txtfullname";
+            this.txtfullname.PlaceholderText = "";
+            this.txtfullname.SelectedText = "";
+            this.txtfullname.Size = new System.Drawing.Size(546, 94);
+            this.txtfullname.TabIndex = 0;
+            // 
+            // txtage
+            // 
+            this.txtage.BackColor = System.Drawing.Color.Transparent;
+            this.txtage.BorderColor = System.Drawing.Color.LawnGreen;
+            this.txtage.BorderRadius = 10;
+            this.txtage.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.txtage.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtage.DefaultText = "";
+            this.txtage.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtage.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtage.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtage.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtage.FillColor = System.Drawing.Color.Azure;
+            this.txtage.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtage.Font = new System.Drawing.Font("Garamond", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtage.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtage.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtage.Location = new System.Drawing.Point(125, 209);
+            this.txtage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtage.Name = "txtage";
+            this.txtage.PlaceholderText = "";
+            this.txtage.SelectedText = "";
+            this.txtage.Size = new System.Drawing.Size(546, 94);
+            this.txtage.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label1.Location = new System.Drawing.Point(123, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Full Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label2.Location = new System.Drawing.Point(123, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label3.Location = new System.Drawing.Point(123, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nationality";
+            // 
+            // txtnationality
+            // 
+            this.txtnationality.BackColor = System.Drawing.Color.Transparent;
+            this.txtnationality.BorderColor = System.Drawing.Color.LawnGreen;
+            this.txtnationality.BorderRadius = 10;
+            this.txtnationality.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.txtnationality.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtnationality.DefaultText = "";
+            this.txtnationality.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtnationality.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtnationality.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtnationality.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtnationality.FillColor = System.Drawing.Color.Azure;
+            this.txtnationality.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtnationality.Font = new System.Drawing.Font("Garamond", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnationality.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtnationality.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtnationality.Location = new System.Drawing.Point(125, 354);
+            this.txtnationality.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtnationality.Name = "txtnationality";
+            this.txtnationality.PlaceholderText = "";
+            this.txtnationality.SelectedText = "";
+            this.txtnationality.Size = new System.Drawing.Size(546, 94);
+            this.txtnationality.TabIndex = 4;
+            // 
+            // txtcivilstatus
+            // 
+            this.txtcivilstatus.BackColor = System.Drawing.Color.Transparent;
+            this.txtcivilstatus.BorderColor = System.Drawing.Color.LawnGreen;
+            this.txtcivilstatus.BorderRadius = 10;
+            this.txtcivilstatus.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.txtcivilstatus.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtcivilstatus.DefaultText = "";
+            this.txtcivilstatus.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtcivilstatus.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtcivilstatus.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtcivilstatus.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtcivilstatus.FillColor = System.Drawing.Color.Azure;
+            this.txtcivilstatus.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtcivilstatus.Font = new System.Drawing.Font("Garamond", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcivilstatus.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtcivilstatus.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtcivilstatus.Location = new System.Drawing.Point(125, 496);
+            this.txtcivilstatus.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtcivilstatus.Name = "txtcivilstatus";
+            this.txtcivilstatus.PlaceholderText = "";
+            this.txtcivilstatus.SelectedText = "";
+            this.txtcivilstatus.Size = new System.Drawing.Size(546, 94);
+            this.txtcivilstatus.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label4.Location = new System.Drawing.Point(123, 468);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Civil Status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label5.Location = new System.Drawing.Point(123, 611);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Birthdate";
+            // 
+            // txtbirthdate
+            // 
+            this.txtbirthdate.BackColor = System.Drawing.Color.Transparent;
+            this.txtbirthdate.BorderColor = System.Drawing.Color.LawnGreen;
+            this.txtbirthdate.BorderRadius = 10;
+            this.txtbirthdate.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.txtbirthdate.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtbirthdate.DefaultText = "";
+            this.txtbirthdate.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtbirthdate.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtbirthdate.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtbirthdate.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtbirthdate.FillColor = System.Drawing.Color.Azure;
+            this.txtbirthdate.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtbirthdate.Font = new System.Drawing.Font("Garamond", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbirthdate.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtbirthdate.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtbirthdate.Location = new System.Drawing.Point(125, 639);
+            this.txtbirthdate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtbirthdate.Name = "txtbirthdate";
+            this.txtbirthdate.PlaceholderText = "";
+            this.txtbirthdate.SelectedText = "";
+            this.txtbirthdate.Size = new System.Drawing.Size(546, 94);
+            this.txtbirthdate.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(127, 764);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(359, 71);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Display";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.clearbtn.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearbtn.ForeColor = System.Drawing.Color.DarkGreen;
+            this.clearbtn.Location = new System.Drawing.Point(505, 764);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(166, 71);
+            this.clearbtn.TabIndex = 10;
+            this.clearbtn.Text = "Clear";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            this.clearbtn.Click += new System.EventHandler(this.clearbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 895);
+            this.Controls.Add(this.clearbtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtbirthdate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtcivilstatus);
+            this.Controls.Add(this.txtnationality);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtage);
+            this.Controls.Add(this.txtfullname);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Personal Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txtfullname;
+        private Guna.UI2.WinForms.Guna2TextBox txtage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox txtnationality;
+        private Guna.UI2.WinForms.Guna2TextBox txtcivilstatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2TextBox txtbirthdate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clearbtn;
+    }
+}
+
