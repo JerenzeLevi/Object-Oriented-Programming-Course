7d7b217e4554c77f9c11e61859f9fb7538b7b6ab
The Knave <omandamjerenze@gmail.com>
2026-06-11 17:19:13 +0800

    Added BookApp.cs

---
+++ Midterm/Omandam_Midterm_SetA/Omandam_Midterm_SetA/BookApp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omandam_Midterm_SetA
+{
+    class BookApp
+    {
+        private String Title;
+        private String Author;
+        private String Publisher;
+        private int YearPublished;
+
+        public BookApp(String title, String author, String publisher, int yearPublisher){
+            this.Title = title;
+            this.Author = author;
+            this.Publisher = publisher;
+            this.YearPublished = yearPublisher;
+        }
+
+        public String title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+
+        public String author
+        {
+            get { return Author; }
+            set { Author = value; }
+        }
+
+        public String publisher
+        {
+            get { return Publisher; }
+            set { Publisher = value; }
+        }
+
+        public int yearpublished
+        {
+            get { return YearPublished; }
+            set { YearPublished = value; }
+        }
+    }
+
+    
+}
