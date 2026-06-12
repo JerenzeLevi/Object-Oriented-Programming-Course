bdc647e87fc8c6fd8a492d97d39a11ed18d20fe1
The Knave <omandamjerenze@gmail.com>
2026-06-12 22:26:45 +0800

    Added a file

---
+++ Midterm/Information_Management_System/Information_Management_System/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Information_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplayInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDisplayInfo
+            // 
+            this.btnDisplayInfo.Font = new System.Drawing.Font("Rockwell", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplayInfo.Location = new System.Drawing.Point(139, 298);
+            this.btnDisplayInfo.Name = "btnDisplayInfo";
+            this.btnDisplayInfo.Size = new System.Drawing.Size(507, 70);
+            this.btnDisplayInfo.TabIndex = 0;
+            this.btnDisplayInfo.Text = "Display Personal Information";
+            this.btnDisplayInfo.UseVisualStyleBackColor = true;
+            this.btnDisplayInfo.Click += new System.EventHandler(this.btnDisplayInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDisplayInfo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Information Management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplayInfo;
+    }
+}
+
