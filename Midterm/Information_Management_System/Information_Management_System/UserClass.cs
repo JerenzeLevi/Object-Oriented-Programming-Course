19b046c635bb33a2a8456138168d351c3c485e06
The Knave <omandamjerenze@gmail.com>
2026-06-12 22:23:56 +0800

    Added a file

---
+++ Midterm/Information_Management_System/Information_Management_System/UserClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Information_Management_System
+{
+    class UserClass
+    {
+        int userId = 301594;
+        string userName = "Jerenze Levi Arlecchino T. Omandam";
+        int userAge = 18;
+        string userAddress = "Osaka, Japan";
+
+        public void displayInfo()
+        {
+            MessageBox.Show("\t=== Your Personal Information ===\nUser ID: " + userId + "\nName: " + userName + "\nAge: " + userAge + "\nAddress: " + userAddress);
+        }
+    }
+}
