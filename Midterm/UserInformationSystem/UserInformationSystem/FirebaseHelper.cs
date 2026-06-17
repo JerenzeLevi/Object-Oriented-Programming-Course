f3aae9b4bbaf32beb1d6ee7e1d06c6c51a8630f0
The Knave <omandamjerenze@gmail.com>
2026-06-17 18:51:03 +0800

    Added a file

---
+++ Midterm/UserInformationSystem/UserInformationSystem/FirebaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UserInformationSystem
+{
+    class FirebaseHelper
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        //REPLACE THIS LINK BELOW, DON'T YA THINK OF DOING SMTH WEIRD OR I SLAP YO BUTTCHEEKS WHEN I CATCH YOU
+        private static readonly string baseUrl = "https://oopdatabase-671fc-default-rtdb.firebaseio.com/";
+
+        //save Data
+
+        public static async Task SaveDataAsync(string path, object data)
+        {
+            string json = JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PutAsync(baseUrl + path + ".json", content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        //get Data
+
+
+    }
+}
