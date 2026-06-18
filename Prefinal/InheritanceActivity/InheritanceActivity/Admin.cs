692e8edbd89527c8d8db5485fa02ebe5ad46f8ed
The Knave <omandamjerenze@gmail.com>
2026-06-18 20:20:49 +0800

    Added a file

---
+++ Prefinal/InheritanceActivity/InheritanceActivity/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceActivity
+{
+    class Admin : User
+    {
+        public override string GetRole()
+        {
+            return "Administrator";
+        }
+    }
+}
