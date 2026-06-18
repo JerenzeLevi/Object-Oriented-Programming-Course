e4d40b7af9e6463cd9ee72bba570e7daf6a2ce0a
The Knave <omandamjerenze@gmail.com>
2026-06-18 20:12:48 +0800

    Added a file

---
+++ Prefinal/InheritanceActivity/InheritanceActivity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceActivity
+{
+    class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual string GetRole()
+        {
+            return "User";
+        }
+    }
+}
