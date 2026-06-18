a8f132c5814fefcf527e3736cfb43aad0169d9b3
The Knave <omandamjerenze@gmail.com>
2026-06-18 20:13:24 +0800

    Added a file

---
+++ Prefinal/InheritanceActivity/InheritanceActivity/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InheritanceActivity
+{
+    class Teacher : User
+    {
+        public string Subject { get; set; }
+
+        public override string GetRole()
+        {
+            return "Teacher";
+        }
+    }
+}
